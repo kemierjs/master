fdaf8ecb4711579943c5d3ceb6f7be3ccd771ff1
v_choczhang <v_choczhang@tencent.com>
2022-06-10 19:34:55 +0800

    add code

--- nodejs/test/main.js
+++ nodejs/test/main.js
@@ -1,32 +1,12 @@
-function Node(name) {
-    this.left = null;
-    this.right = null;
-    this.name = name
-    this.preEach = () => {
-        console.log(this)
-        if (!this.left) {
-            this.left.preEach();
-        }
-        if (!this.right) {
-            this.right.preEach();
-        }
-
-    };
+async function test() {
+    return new Promise((resolve, reject) => {
+        console.log(32423)
+        resolve();
+    });
 }
 
-let root = new Node('root');
-let l = new Node('l');
-let r = new Node('r');
-let ll = new Node('ll');
-let lr = new Node('lr');
-let rl = new Node('rl');
-let rr = new Node('rr');
-root.left = l;
-root.right = r;
-l.left = ll;
-l.right = lr;
-r.left = rl;
-r.right = rr;
-console.log(root)
-
-root.preEach();
+test().then(value => {
+    console.log(value)
+},(value) => {
+    console.log(value)
+});
